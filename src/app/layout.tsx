8fe085d5f0e0fd91f2104185109e11e9398f2445
niloymajumder <niloymajumderr@gmail.com>
2026-02-19 12:49:46 +0600

    Add Umami analytics tracking script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        <script defer src="https://cloud.umami.is/script.js" data-website-id="8fd43d7e-8063-489b-9f3a-2314829acc53"></script>
       </head>
       <body className="font-sans antialiased">
         {children}
