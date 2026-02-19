b70076b62ae196a4eeac7c5c43f4cf501d03d1cd
niloymajumder <niloymajumderr@gmail.com>
2026-02-19 12:28:40 +0600

    Add maintenance page with Umami tracking script

---
+++ src/app/maintenance/page.tsx
@@ -0,0 +1,15 @@
+import Script from 'next/script';
+import MaintenanceSection from '@/components/sections/maintenance-section';
+
+export default function MaintenancePage() {
+  return (
+    <>
+      <Script
+        src="https://cloud.umami.is/script.js"
+        data-website-id="8fd43d7e-8063-489b-9f3a-2314829acc53"
+        strategy="lazyOnload"
+      />
+      <MaintenanceSection />
+    </>
+  );
+}
